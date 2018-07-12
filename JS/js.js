a7370ce4811c8102a254f6db35f111dad79f9d03
Glumac7 <necaglumicic@gmail.com>
2018-07-12 12:47:01 +0200

    Added the ability to add new tasks

--- JS/js.js
+++ JS/js.js
@@ -1,7 +1,16 @@
 function add_button_clicked()
 {
-    localStorage.setItem("asd", "dsa")
-    console.log(localStorage.getItem("asd"));
+    let id = 0;
+
+    let white_line = document.getElementById('add-container-bottom');
+    white_line.style.display = "block";
+        
+    let content = document.getElementsByTagName('input')[0].value;
+    let setDiv = document.getElementById('container').innerHTML += `<div id="display-content"><p id="main-p">${content}</p></div>`;
+
+    localStorage.setItem(`${id}`, setDiv);
+
+    id++;
+
+    document.getElementsByTagName('input')[0].value = "";
 }
-/*Use the localStorage for saving data even after the browser closes, it retursn .toString
-so be careful, PS. Don't fuck it up!*/
